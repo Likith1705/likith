211fb29563687ecc32529930d425bb16119add5e
Ubuntu <ubuntu@ip-172-31-40-12.ap-south-1.compute.internal>
2022-09-23 13:01:44 +0000

    mail script is added

--- email.sh
+++ email.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-perc=`df -h . | awk -F " " 'NR==2{print $5}' | sed 's/%/ /g'`
-if [ $perc -ge 18 ]; then
-        echo "/dev/root drive size reached maximum thershold value and current usage is $perc percent" | mail -s 'disk usage' likithbs703@gmail.com
+perc=`df -h . | awk -F " " 'NR==2{print$5}' | sed 's/%/ /g'`
+if [ $perc -ge 25 ];then
+	echo "the memory reached threshold value" | mail ~s "Disk usage" likithbs703@gmail.com
 fi
