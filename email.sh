66cf89650d61689c0911dd1ab93ed1d291086781
Ubuntu <ubuntu@ip-172-31-40-12.ap-south-1.compute.internal>
2022-09-23 16:20:55 +0000

    one file deleted

--- email.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-perc=`df -h . | awk -F " " 'NR==2{print$5}' | sed 's/%/ /g'`
-if [ $perc -ge 25 ];then
-	echo "the memory reached threshold value" | mail ~s "Disk usage" likithbs703@gmail.com
-fi
